df431d81d6524dc414061a4c39152378fb622eb5
luispedro <luispedro@5128b7d6-d249-4073-bd28-aebf1259cb0a>
2008-06-15 00:10:29 +0000

    Make this into a python package
    git-svn-id: file:///home/luispedro/svnroot/mmorph/trunk@51 5128b7d6-d249-4073-bd28-aebf1259cb0a

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='pymorph',
+      version='0.88',
+      description='Image Morphology Toolbox',
+      author='Luís Pedro Coelho',
+      author_email='lpc@mcu.edu',
+      url='http://luispedro.org/pymorph/',
+      packages=find_packages(),
+      )
+
+
