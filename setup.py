8c1a66928503ef07983a8db7ebbb715ecc9f4f61
luispedro <luispedro@5128b7d6-d249-4073-bd28-aebf1259cb0a>
2008-08-14 21:21:58 +0000

    Long description
    git-svn-id: file:///home/luispedro/svnroot/mmorph/trunk@75 5128b7d6-d249-4073-bd28-aebf1259cb0a

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+long_description='''Image Morphology Toolbox
+
+The image morphology toolbox implements the basic binary and
+grayscale morphology operations, working with numpy arrays to
+hold image data.
+
+This is a pure Python package which is the companion package
+to the book "Hands-on Morphological Image Processing."
+'''
+
 setup(name='pymorph',
       version='0.89',
       description='Image Morphology Toolbox',
+      long_description=long_description,
       author='Luis Pedro Coelho',
       author_email='lpc@mcu.edu',
       url='http://luispedro.org/pymorph/',
+      license='BSD',
       packages=find_packages(),
       )
 
