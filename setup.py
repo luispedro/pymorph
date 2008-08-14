65d3026c40bdb43f984eb9c0fd6025817ca50d37
luispedro <luispedro@5128b7d6-d249-4073-bd28-aebf1259cb0a>
2008-08-14 21:17:41 +0000

    pypi does not like UTF-8
    git-svn-id: file:///home/luispedro/svnroot/mmorph/trunk@74 5128b7d6-d249-4073-bd28-aebf1259cb0a

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='pymorph',
       version='0.89',
       description='Image Morphology Toolbox',
-      author='Luís Pedro Coelho',
+      author='Luis Pedro Coelho',
       author_email='lpc@mcu.edu',
       url='http://luispedro.org/pymorph/',
       packages=find_packages(),
