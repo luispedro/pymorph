14549b96e95040d8e9502a47814189805e5d8a8f
Luís Pedro Coelho <lpc@cmu.edu>
2009-07-08 19:01:43 -0400

    Increase version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classifiers = [
     ]
 
 setup(name='pymorph',
-      version='0.91.1-git',
+      version='0.91.2',
       description='Image Morphology Toolbox',
       long_description=long_description,
       author='Luis Pedro Coelho',
