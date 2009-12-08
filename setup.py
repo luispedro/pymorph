5aad6bab325bec67701c65176f24de1be14d6afb
Luís Pedro Coelho <lpc@cmu.edu>
2009-12-08 00:55:37 -0500

    Up the version number
    A couple of documentation fixes

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classifiers = [
     ]
 
 setup(name='pymorph',
-      version='0.91.5',
+      version='0.91.5-git',
       description='Image Morphology Toolbox',
       long_description=long_description,
       author='Luis Pedro Coelho',
