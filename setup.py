339a10cfbdd523a041d6b768cd819fec20c2af2c
Luís Pedro Coelho <lpc@cmu.edu>
2009-12-13 17:09:33 -0500

    Better long_description
    PyPI at least uses this text.

--- setup.py
+++ setup.py
@@ -8,7 +8,28 @@ greyscale morphology operations, working with numpy arrays
 representing
 
 This is a pure Python package which is the companion package
-to the book "Hands-on Morphological Image Processing."
+to the book "Hands-on Morphological Image Processing." It has been
+updated to work with numpy and the names and interfaces have been
+Pythonised.
+
+Includes basic operations such as
+    - erode
+    - dilate
+    - open
+    - tophat opening
+    - watershed
+    - ...
+
+*Website*: `http://luispedro.org/software/pymorph <http://luispedro.org/software/pymorph>`_
+
+Future Plans
+~~~~~~~~~~~~
+
+When all functions have a corresponding unit test and the whole of the
+API documentation has been re-written into numpy rst format, I will release
+version 1.0.
+
+The package is actively maintained and any reported bug will be fixed fast.
 '''
 classifiers = [
     'Development Status :: 5 - Production/Stable',
