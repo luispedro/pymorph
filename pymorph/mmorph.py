73b549a557c68d1e3209f9f10606daacda65ff9c
Luís Pedro Coelho <lpc@cmu.edu>
2010-01-02 00:34:05 -0500

    Move not implemented functions into their own file
    This also prevents them from being imported into the pymorph namespace
    and confusing users.

--- pymorph/mmorph.py
+++ pymorph/mmorph.py
@@ -98,7 +98,6 @@ pattern recognition and image analysis.
 - `supcanon()`     : Union of sup-generating or hit-miss operators.
 - `supgen()`       : Sup-generating (hit-miss).
 - `suprec()`       : Sup-reconstruction.
-- `swatershed()`   : Detection of similarity-based watershed from markers.
 - `symdif()`       : Symmetric difference between two images
 - `text()`         : Create a binary image of a text.
 - `thick()`        : Image transformation by thickening.
@@ -106,7 +105,6 @@ pattern recognition and image analysis.
 - `threshad()`     : Threshold (adaptive)
 - `toggle()`       : Image contrast enhancement or classification by the toggle operator.
 - `union()`        : Union of images.
-- `vmax()`         : Remove domes with volume less than v.
 - `watershed()`    : Watershed detection.
 - `to_int32()`     : Convert an image to an int32 image.
 - `to_uint16()`    : Convert an image to a uint16 image.
@@ -114,9 +112,9 @@ pattern recognition and image analysis.
 
 """
 from __future__ import division
-__version__ = '0.91.4'
+__version__ = '0.92.2'
 
-__build_date__ = '12 November 2009'
+__build_date__ = '1 January 2010'
 
 
 import sys, os
@@ -1899,31 +1897,6 @@ def hmin(f, h=1, Bc=None):
     if Bc is None: Bc = secross()
     g = addm(f, h)
     return suprec(g, f, Bc);
-
-
-def vmax(f, v=1, Bc=None):
-    """
-    y = vmax(f, v=1, Bc={3x3 cross})
-
-    Remove domes with volume less than v.
-
-    This operator removes connected domes with volume less
-    than `v`. This function is very similar to `hmax`, but instead
-    of using a gray scale criterion (contrast) for the dome, it uses
-    a volume criterion.
-
-    Parameters
-    ----------
-      f :  Gray-scale (uint8 or uint16) image.
-      v :  Volume parameter (default: 1).
-      Bc : Structuring element (default: 3x3 cross).
-    Returns
-    -------
-      y : Gray-scale (uint8 or uint16) or binary image.
-    """
-
-    if Bc is None: Bc = secross()
-    raise NotImplementedError, 'Not implemented yet'
 
 
 def hmax(f, h=1, Bc=None):
@@ -3763,58 +3736,6 @@ def bshow(f1, f2=None, f3=None, factor=17):
             if f3 and f3[i,j]:
                 y[m:m+s, n:n+s] += fillrect
     return (y > 0)
-
-
-def swatershed(f, g, B=None, LINEREG="LINES"):
-    """
-        - Purpose
-            Detection of similarity-based watershed from markers.
-        - Synopsis
-            y = swatershed(f, g, B=None, LINEREG="LINES")
-        - Input
-            f:       Gray-scale (uint8 or uint16) image.
-            g:       Gray-scale (uint8 or uint16) or binary image. Marker
-                     image. If binary, each connected component is an object
-                     marker. If gray, it is assumed it is a labeled image.
-            B:       Structuring Element Default: None (3x3 elementary
-                     cross). (watershed connectivity)
-            LINEREG: String Default: "LINES". 'LINES' or ' REGIONS'.
-        - Output
-            y: Gray-scale (uint8 or uint16) or binary image.
-        - Description
-            swatershed creates the image y by detecting the domain of the
-            catchment basins of f indicated by g , according with the
-            connectivity defined by B . This watershed is a modified version
-            where each basin is defined by a similarity criterion between
-            pixels. The original watershed is normally applied to the
-            gradient of the image. In this case, the gradient is taken
-            internally. According to the flag LINEREG y will be a labeled
-            image of the catchment basins domain or just a binary image that
-            presents the watershed lines. The implementation of this
-            function is based on LotuFalc:00 .
-        - Examples
-            #
-            f = to_uint8([
-                [0,  0,  0,  0,  0,  0,  0],
-                [0,  1,  0,  0,  0,  1,  0],
-                [0,  1,  0,  0,  0,  1,  0],
-                [0,  1,  1,  1,  1,  1,  0],
-                [0,  1,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  0]])
-            m = to_uint8([
-                [0,  0,  0,  0,  0,  0,  0],
-                [0,  1,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  0,  0,  0,  0],
-                [0,  0,  0,  2,  0,  0,  0]])
-            print swatershed(f,m,secross(),'REGIONS')
-    """
-
-    if B is None: B = secross()
-    print 'Not implemented yet'
-    return None
-    return y
 
 
 def symdif(f1, f2):
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ classifiers = [
     ]
 
 setup(name='pymorph',
-      version='0.92.1',
+      version='0.92.2',
       description='Image Morphology Toolbox',
       long_description=long_description,
       author='Luis Pedro Coelho',
