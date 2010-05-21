006c26e530aa833222d73af9124eb4556c8e4c74
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-20 20:00:14 -0400

    More testing of erode()/dilate()

--- tests/test_dilate.py
+++ tests/test_dilate.py
@@ -1,7 +1,7 @@
 import pymorph
 import numpy as np
 
-def test_erode():
+def test_dilate():
     f = np.zeros((8,8), np.bool)
     Bs = [np.reshape(B, (3,3)) for B in (
                     [1,1,0, 1,1,0, 0,0,0],
@@ -10,13 +10,5 @@ def test_erode():
                     [0,1,0, 0,1,0, 0,0,0],
                     )]
     for B in Bs:
-        assert pymorph.erode(f, B != 0).sum() == 0
-        assert pymorph.erode(f, B).sum() == 0
-
-def test_erode_mixed_types():
-    A  = np.array([[1,0],[0,1]])
-    f = np.zeros((4,4), np.bool)
-    f[2,2] = 1
-    f[3,3] = 1
-    assert pymorph.erode(f,A).sum() == 1
-
+        assert pymorph.dilate(f, B != 0).sum() == 0
+        assert pymorph.dilate(f, B).sum() == 0
