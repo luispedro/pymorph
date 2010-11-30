a4104dea137b8fa2aedc38ac3bda53c559c1f45a
Luis Pedro Coelho <lpc@cmu.edu>
2010-11-30 00:20:47 -0500

    TST: Test all cases of type for opentransf

--- tests/test_opentransf.py
+++ tests/test_opentransf.py
@@ -17,9 +17,21 @@ def test_opentransf():
                 t = pymorph.open(f, pymorph.sedisk(k, 2, 'city-block'))
             assert not t[y,x]
 
-def test_all():
+def test_all_types():
     f = np.arange(9).reshape((3,3)) % 3 > 0
     # linear-h crashed in 0.95
     # and was underlying cause of crash in patsec(f, 'linear-h')
-    g = pymorph.opentransf(f, 'linear-h')
 
+    def test_type(type, Buser):
+        g = pymorph.opentransf(f, type, Buser=Buser)
+
+    yield test_type, 'linear-h', None
+    yield test_type, 'octagon', None
+    yield test_type, 'chessboard', None
+    yield test_type, 'city-block', None
+    yield test_type, 'linear-v', None
+    yield test_type, 'linear-45r', None
+    yield test_type, 'linear-45l', None
+    Buser = np.ones((3,3),bool)
+    Buser[2,2] = 0
+    yield test_type, 'user', Buser
