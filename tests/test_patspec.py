6dd546d97710c99201af17c19e0f48a8c4702f72
Luis Pedro Coelho <lpc@cmu.edu>
2010-11-29 21:25:15 -0500

    TST: Test case for newly reported bug
    This was reported by Alexandre Harano.

--- tests/test_patspec.py
+++ tests/test_patspec.py
@@ -9,3 +9,9 @@ def test_patspec():
         [1,1,0,0,0,0,0,0]], bool)
     assert pymorph.patspec(f).sum() == (f > 0).sum()
 
+def test_linear_h():
+    f = np.arange(9).reshape((3,3)) % 3 > 0
+    # This crashed in 0.95
+    # reported by Alexandre Harano
+    g = pymorph.patspec(f, 'linear-h')
+
