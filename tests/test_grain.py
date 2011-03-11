5afec98c9db9d3538fdb37302ca52d02549a6675
Luis Pedro Coelho <lpc@cmu.edu>
2011-03-11 13:45:52 -0500

    TST A new test for grain()
    Contributed by Timothy Hirzel

--- tests/test_grain.py
+++ tests/test_grain.py
@@ -1,4 +1,4 @@
-import pymorph 
+import pymorph
 import numpy as np
 def test_grain():
     L = np.array([
@@ -38,3 +38,27 @@ def test_image():
         for v,lab in zip(image.ravel(), labels.ravel()):
             if lab > 0:
                 assert v == data[lab-1]
+
+def test_off_by_one():
+    # Contributed by Timothy Hirzel
+    L = np.array([
+            [0,0,1,1,1,0],
+            [0,1,0,0,0,0],
+            [0,0,2,2,2,0],
+            [0,2,2,2,2,2],
+            ])
+    D = np.array([
+            [0, 0,12,12,12, 0],
+            [0,12, 0, 0, 0, 0],
+            [0, 0,21,22,23, 0],
+            [0,24,25,26,27,28],
+            ])
+    maxdata = np.array([12,28])
+    maximage = np.array([
+            [0, 0,12,12,12, 0],
+            [0,12, 0, 0, 0, 0],
+            [0, 0,28,28,28, 0],
+            [0,28,28,28,28,28],
+            ])
+    assert((maxdata == pymorph.grain(D,L,'max', 'data')).all())
+    assert((maximage == pymorph.grain(D,L,'max', 'image')).all())
